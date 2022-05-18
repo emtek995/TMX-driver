2039fa7e3f59d5c6be62783894b8f643d15249a1
Ed Way <emtek995@gmail.com>
2022-05-18 08:09:09 -0500

    Fix ISO C90 warning

--- hid_tmx.c
+++ hid_tmx.c
@@ -10,10 +10,14 @@
 #define TMX_GIP 0xB67E
 #define TMX_FFB 0xB65D
 
-static void tmx_control_handler(struct urb *urb) {
-    if (urb->status == 0) {
+static void tmx_control_handler(struct urb *urb)
+{
+    if (urb->status == 0)
+    {
         printk("Success!\n");
-    } else {
+    }
+    else
+    {
         printk("Error maybe?\n");
     }
 }
@@ -21,18 +25,6 @@ static void tmx_control_handler(struct urb *urb) {
 static int hid_tmx_probe(struct hid_device *hdev, const struct hid_device_id *id)
 {
     int value = 7;
-    struct usb_device *device = interface_to_usbdev(to_usb_interface(hdev->dev.parent));
-    // Pass 0 for packet size because only sending
-    // to control endpoint
-    struct urb *tmxurb = usb_alloc_urb(0, GFP_KERNEL);
-
-
-    // Wheel goes through 2 stages to init
-    printk("probing for wheel\n");
-    if (id->product != TMX_FFB) {
-        printk("Not a known wheel\n");
-        return -ENODEV;
-    }
 
     struct usb_ctrlrequest packet = {
         .bRequestType = 0x41,
@@ -42,10 +34,23 @@ static int hid_tmx_probe(struct hid_device *hdev, const struct hid_device_id *id
         .wLength = 0
     };
 
+    struct usb_device *device = interface_to_usbdev(to_usb_interface(hdev->dev.parent));
+    // Pass 0 for packet size because only sending
+    // to control endpoint
+    struct urb *tmxurb = usb_alloc_urb(0, GFP_KERNEL);
+
+    // Wheel goes through 2 stages to init
+    printk("probing for wheel\n");
+    if (id->product != TMX_FFB)
+    {
+        printk("Not a known wheel\n");
+        return -ENODEV;
+    }
+
     usb_fill_control_urb(tmxurb, device, usb_sndctrlpipe(device, 0), (unsigned char *)&packet, 0, 0, tmx_control_handler, 0);
-    
+
     usb_submit_urb(tmxurb, GFP_KERNEL);
-    
+
     usb_free_urb(tmxurb);
 
     return 0;
@@ -53,11 +58,11 @@ static int hid_tmx_probe(struct hid_device *hdev, const struct hid_device_id *id
 
 static void hid_tmx_remove(struct hid_device *hdev)
 {
-    printk("disconnect the wheel\n");   
+    printk("disconnect the wheel\n");
 }
 
 static const struct hid_device_id hid_tmx_devices[] = {
-//    {HID_USB_DEVICE(THRUSTMASTER_VID, TMX_GIP)},
+    //    {HID_USB_DEVICE(THRUSTMASTER_VID, TMX_GIP)},
     {HID_USB_DEVICE(THRUSTMASTER_VID, TMX_FFB)},
     {}};
 
@@ -67,6 +72,4 @@ struct hid_driver hid_tmx_driver = {
     .name = "tmx",
     .id_table = hid_tmx_devices,
     .probe = hid_tmx_probe,
-    .remove = hid_tmx_remove
-};
-
+    .remove = hid_tmx_remove};
--- tmx.c
+++ tmx.c
@@ -10,10 +10,14 @@
 #define TMX_GIP 0xB67E
 //#define TMX_FFB 0xB65D
 
-static void tmx_control_handler(struct urb *urb) {
-    if (urb->status == 0) {
+static void tmx_control_handler(struct urb *urb)
+{
+    if (urb->status == 0)
+    {
         printk("Success!\n");
-    } else {
+    }
+    else
+    {
         printk("Error maybe?\n");
     }
 }
@@ -21,31 +25,31 @@ static void tmx_control_handler(struct urb *urb) {
 static int tmx_probe(struct usb_interface *iface, const struct usb_device_id *id)
 {
     int value = 1;
-    struct usb_device *device = interface_to_usbdev(iface);
-    // Pass 0 for packet size because only sending
-    // to control endpoint
-    struct urb *tmxurb = usb_alloc_urb(0, GFP_KERNEL);
-
-
-    // Wheel goes through 2 stages to init
-    printk("probing for wheel\n");
-    if (id->idProduct != TMX_GIP) {
-        printk("Not a known wheel\n");
-        return -ENODEV;
-    }
 
     struct usb_ctrlrequest packet = {
         .bRequestType = 0x41,
         .bRequest = 83,
         .wValue = cpu_to_le16(value),
         .wIndex = 0,
-        .wLength = 0
-    };
+        .wLength = 0};
+
+    struct usb_device *device = interface_to_usbdev(iface);
+    // Pass 0 for packet size because only sending
+    // to control endpoint
+    struct urb *tmxurb = usb_alloc_urb(0, GFP_KERNEL);
+
+    // Wheel goes through 2 stages to init
+    printk("probing for wheel\n");
+    if (id->idProduct != TMX_GIP)
+    {
+        printk("Not a known wheel\n");
+        return -ENODEV;
+    }
 
     usb_fill_control_urb(tmxurb, device, usb_sndctrlpipe(device, 0), (unsigned char *)&packet, 0, 0, tmx_control_handler, 0);
-    
+
     usb_submit_urb(tmxurb, GFP_KERNEL);
-    
+
     usb_free_urb(tmxurb);
 
     return 0;
@@ -53,12 +57,12 @@ static int tmx_probe(struct usb_interface *iface, const struct usb_device_id *id
 
 static void tmx_disconnect(struct usb_interface *iface)
 {
-    printk("disconnect the wheel\n");   
+    printk("disconnect the wheel\n");
 }
 
 static const struct usb_device_id tmx_devices[] = {
     {USB_DEVICE(THRUSTMASTER_VID, TMX_GIP)},
-//    {USB_DEVICE(THRUSTMASTER_VID, TMX_FFB)},
+    //    {USB_DEVICE(THRUSTMASTER_VID, TMX_FFB)},
     {}};
 
 MODULE_DEVICE_TABLE(usb, tmx_devices);
@@ -67,22 +71,25 @@ static struct usb_driver tmx_driver = {
     .name = "tmx",
     .id_table = tmx_devices,
     .probe = tmx_probe,
-    .disconnect = tmx_disconnect
-};
+    .disconnect = tmx_disconnect};
 
 extern struct hid_driver hid_tmx_driver;
 
-static int __init tmx_init(void) {
-    if (0 > usb_register(&tmx_driver)) {
+static int __init tmx_init(void)
+{
+    if (0 > usb_register(&tmx_driver))
+    {
         return -1;
     }
-    if (0 > hid_register_driver(&hid_tmx_driver)) {
+    if (0 > hid_register_driver(&hid_tmx_driver))
+    {
         return -1;
     }
     return 0;
 }
 
-static void __exit tmx_exit(void) {
+static void __exit tmx_exit(void)
+{
     hid_unregister_driver(&hid_tmx_driver);
     usb_deregister(&tmx_driver);
 }
